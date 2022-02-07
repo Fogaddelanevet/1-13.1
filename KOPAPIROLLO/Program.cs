77d1dc3d618feb717714af245de1257335800f8d
Fogaddelanevet <90307397+Fogaddelanevet@users.noreply.github.com>
2022-02-07 14:49:48 +0100

    OPOLO

---
+++ KOPAPIROLLO/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KOPAPIROLLO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            string gepdont = "";
+            string jatekos = "";
+
+            int geppont=0;
+            int jatekospont=0;
+            
+            bool l = true;
+
+            do
+            {
+                Console.WriteLine("Akarsz játszani? i/n");
+                if (Console.ReadKey(true).KeyChar == 'n')
+                {
+                    l = false;
+                    
+                }
+            }
+            while (l==true);
+            {
+                Console.WriteLine("Mit választasz? (k/p/o)");
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'k': jatekos = "kő";
+                        break;
+                    case 'p': jatekos = "papír";
+                        break;
+                    case 'o': jatekos = "olló";
+                        break;
+                }
+                switch (r.Next(0, 3))
+                {
+                    case 0: gepdont = "kő";
+                        break; 
+                    case 1: gepdont = "papír";
+                        break;
+                    case 2: gepdont = "olló";
+                        break;
+                }
+                if ((jatekos == "kő" && gepdont == "papír")
+                    || 
+                    (jatekos == "papír" && gepdont == "olló")
+                    || 
+                    (jatekos == "olló" && gepdont == "kő"))
+                { 
+                    Console.WriteLine("Veszítettél! Az állás:\nSzámítógép: {0}\nJátékos:{1}", ++geppont, jatekospont);
+                }
+                else if(jatekos == gepdont)
+                    { Console.WriteLine("Döntetlen! Az állás:\nSzámítógép: {0}\nJátékos:{1}", geppont, jatekospont);
+                } 
+                else
+                { 
+                    Console.WriteLine("Nyertél! Az állás:\nSzámítógép: {0}\nJátékos:{1}", geppont, ++jatekospont);
+                }
+
+
+            }
+        }
+    }
+}
--- nev/Program.cs
+++ nev/Program.cs
@@ -10,7 +10,45 @@ namespace nev
     {
         static void Main(string[] args)
         {
+            int number,num2; 
+            
 
+            if (args.Length == 0) 
+            {
+                number = Convert.ToInt32(Console.ReadLine());
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+            }
+            if (args.Length==0)
+            {
+                Random r = new Random(); 
+                number = r.Next(100);
+                num2 = r.Next(100);
+            }
+            else 
+            { 
+                number = int.Parse(args[0]);
+                num2 = int.Parse(args[0]);
+            }
+            Console.WriteLine(number * num2);
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+
+
+
+
+
+
+        
         }
     }
-}
+
