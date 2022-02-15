36298be8e2297dcec3c2a4b6fd43fdb8a5c936a9
Fogaddelanevet <90307397+Fogaddelanevet@users.noreply.github.com>
2022-02-15 11:39:51 +0100

    ss

---
+++ számkitalálós/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace számkitalálós
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int alsoertek = 1, felsoertek = 100, tipp, geptipp, jatekostipp;
+            Random r = new Random();
+
+            Console.Write("akarsz kezdeni?i/n");
+            Console.ReadKey();
+            if(Console.ReadKey().KeyChar=='n')
+            {
+
+            }
+            
+            
+                
+            
+        }
+    }
+}
