1290ec49b76b37817bc6a0b6138864a17430f6cd
Fogaddelanevet <90307397+Fogaddelanevet@users.noreply.github.com>
2022-02-23 10:39:18 +0100

    asasasa

---
+++ masodikharmadik/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace masodikharom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kezdo = 0, veg = 0;
+            Console.WriteLine("ird be a kezdoerteket:");
+            kezdo = int.Parse(Console.ReadLine());
+            Console.WriteLine("ird be a vegerteket:");
+            veg = int.Parse(Console.ReadLine());
+
+            for (int i = kezdo; i < veg; i++)
+            {
+                if (i % 5 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
