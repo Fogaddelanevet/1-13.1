c435f91e7b696877c7cb1d495e36a3d1ce303468
Fogaddelanevet <90307397+Fogaddelanevet@users.noreply.github.com>
2022-03-01 14:29:40 +0100

    asas

--- obijektum/Program.cs
+++ obijektum/Program.cs
@@ -14,14 +14,16 @@ namespace obijektum
             string fajta;
             string szin;
             int kor;
+            string hang;
 
-            public Allat(string nev,string fajta,string szin)
+            public Allat(string nev, string fajta, string szin)
             {
                 this.nev = nev;
                 this.fajta = fajta;
                 this.szin = szin;
-               
                 
+
+
             }
             public Allat()
             {
@@ -49,24 +51,49 @@ namespace obijektum
 
             public void setKor(int p)
             {
-                if(p>0 && p <50)
+                if (p > 0 && p < 50)
                 {
                     this.kor = p;
-                    
-                }
-                
-            }
 
+                }
+
+            }
+            public void setNev(string p)
+            {
+                this.nev = p;
+            }
+            public void setFajta(string p)
+            {
+                this.fajta = p;
+            }
+            public void setSzin(string p)
+            {
+               this.szin = p;
+            }
+            public string hangotAd()
+            {
+                return this.hang;
+            }
         }
         static void Main(string[] args)
         {
             Allat allat1 = new Allat();
-           // allat1.setKor(4);
-           
-            Console.WriteLine("{0} {1} {2} {3}",allat1.getKor(),allat1.getNev(),allat1.getFajta(),allat1.getSzin());
-           
-           // Allat allat2 = new Allat("kakás", "pibull", "black and yellow");
+            // allat1.setKor(4);
+
+            Console.WriteLine("{0} {1} {2} {3}", allat1.getKor(), allat1.getNev(), allat1.getFajta(), allat1.getSzin());
+
+            // Allat allat2 = new Allat("kakás", "pibull", "black and yellow");
             //Console.WriteLine(allat2.getNev());
+            Allat allat3 = new Allat();
+            allat3.setFajta("puli");
+            allat3.setKor(3);
+            allat3.setNev("sanyi");
+            allat3.setSzin("fekete");
+
+
+            Console.WriteLine("{0} {1} {2} {3}", allat3.getKor(), allat3.getNev(), allat3.getSzin(),allat3.getFajta());
+            Console.WriteLine("hang {0} ",allat3.hangotAd());
+
             Console.ReadKey();
         }
     }
