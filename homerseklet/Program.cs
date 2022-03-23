0c3c532fbe017d87bc3334e0a55efd94efed7bec
Fogaddelanevet <90307397+Fogaddelanevet@users.noreply.github.com>
2022-03-23 13:24:13 +0100

    SSDSDS

---
+++ homerseklet/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace honapok
+{
+    class Honapok
+
+    {
+
+        string honap;
+        int nap;
+        int[,] homersekletek = new int[31, 2];
+        Dictionary<string, int> honapok = new Dictionary<string, int>
+        {{"Január",1},{"Február",2 },{"Március",3 },{"Április",4},{"Május",5},{"Június",6 },{"Julius",7},{"Augusztus",8 },{"Szeptember",9 },{"Október",10 },{"November",11 },{"December",12 } };
+        public Honapok()
+        {
+            beolvasas();
+        }
+        public void beolvasas()
+        {
+            Console.WriteLine("HÓNAP?!");
+            this.honap = Console.ReadLine();
+            Console.WriteLine("NAP?!");
+            this.nap = Convert.ToInt32(Console.ReadLine());
+        }
+        public void Tavasz()
+        {
+            Random r = new Random();
+
+            if (this.honapok[this.honap] >= 3 && this.honapok[this.honap] <= 5)
+            {
+                for (int i = 0; i < this.homersekletek.GetLength(0); i++)
+                {
+
+                    this.homersekletek[i, 0] = r.Next(-10, 0);
+                    this.homersekletek[i, 1] = r.Next(0, 20);
+
+
+                }
+
+            }
+
+        }
+        public void kiiratas()
+        {
+            double atlag = (this.homersekletek[this.nap, 0] + this.homersekletek[this.nap, 1]) / 2;
+            Console.WriteLine("a nap hőmérsékletei: este: {0} nappal {1} atlag {2}", this.homersekletek[this.nap, 0], this.homersekletek[this.nap, 1], atlag);
+        }
+
+
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Honapok a = new Honapok();
+            a.Tavasz();
+            a.kiiratas();
+            Console.ReadKey();
+        }
+    }
+}
