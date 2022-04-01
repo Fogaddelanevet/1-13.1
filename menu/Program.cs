45d00c86b55ba0d22875e1cc5da256411fcfb471
Fogaddelanevet <90307397+Fogaddelanevet@users.noreply.github.com>
2022-04-01 10:27:04 +0200

    Menu

---
+++ menu/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace menu
+{
+    class Menu
+    {
+        string[] menu = new string[] { "Első menüpont", "második menupont", "harmadik menupont", "negyedik menupont", "Kilépés" };
+        int szam;
+        public Menu()
+        {
+
+        }
+        public void beker()
+        {
+            Console.WriteLine("Hanyadik menupontot szeretnéd?");
+            this.szam = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(this.menu[this.szam-1]);
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Menu a = new Menu();
+            a.beker();
+            Console.ReadKey();
+
+        }
+    }
+}
