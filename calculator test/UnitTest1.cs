45f85529d3ce50caf89b7b1bd6e43e6c02a38f3d
Fogaddelanevet <90307397+Fogaddelanevet@users.noreply.github.com>
2022-05-27 09:51:09 +0200

    asdsadasdadas

--- calculator test/UnitTest1.cs
+++ calculator test/UnitTest1.cs
@@ -7,14 +7,39 @@ namespace calculator_test
     [TestClass]
     public class UnitTest1
     {
+        Calculator a = new Calculator();
         [TestMethod]
-        public void TestMethod1()
+
+        public void hibamethodus()
         {
-            Calculator a = new Calculator();
-            double varteredmeny = a.eredmeny();
-            double kapotteredmeny = 20;
-            Assert.AreEqual(kapotteredmeny, varteredmeny);
+
+            double varteredmeny1 = a.eredmeny();
+            double kapotteredmeny1 = 10.0;
+            double eredmeny = a.eredmeny();
+            Assert.AreNotEqual(varteredmeny1, kapotteredmeny1);
 
         }
+        [TestMethod]
+        public void jomethodus()
+        {
+            double varteredmeny = a.eredmeny();
+            double kapotteredmeny = 20.0;
+            double eredmeny = a.eredmeny();
+            Assert.AreEqual(kapotteredmeny, varteredmeny);
+
+
+        }
+        [TestMethod]
+        public void nulla()
+        {
+            double kapotteredmeny = a.eredmeny();
+            double varteredmeny = 0;
+            Assert.AreEqual(varteredmeny, kapotteredmeny);
+        }
+        public void TestMethod1()
+        {
+                       
+        }
+        
     }
 }
