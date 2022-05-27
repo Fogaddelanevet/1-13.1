1306a7a2a2a7ccbc9783b1f6d55fa0d1b591d9d0
Fogaddelanevet <90307397+Fogaddelanevet@users.noreply.github.com>
2022-05-27 09:35:04 +0200

    asd

---
+++ calculator test/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using calculator;
+
+namespace calculator_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Calculator a = new Calculator();
+            double varteredmeny = a.eredmeny();
+            double kapotteredmeny = 20;
+            Assert.AreEqual(kapotteredmeny, varteredmeny);
+
+        }
+    }
+}
