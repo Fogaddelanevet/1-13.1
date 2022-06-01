1305110420b36565213de7fe6ef3d1498e7916b8
Fogaddelanevet <90307397+Fogaddelanevet@users.noreply.github.com>
2022-06-01 13:16:43 +0200

    assaas

---
+++ Halmazok/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Halmazok
+{
+    public partial class Form1 : Form
+    {
+        int [] a;
+        int [] b;
+        Random r = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             int a1 = Convert.ToInt32(textBox1.Text);
+             int b1 = Convert.ToInt32(textBox2.Text);
+            a = new int[a1];
+            b = new int[b1];
+
+
+
+          
+            for (int i = 0; i < a.Length; i++)
+            {
+
+               int elem = r.Next(1, 10);
+
+               if(elem!=a[i])
+                {
+                    a[i] = elem;
+                    listBox1.Items.Add(a[i]);
+                   
+                }
+
+            }
+          
+            for (int i = 0; i < b.Length; i++)
+            {
+                b[i] = r.Next(1, 10);
+                listBox2.Items.Add(b[i]);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ futsidejukomp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace futsidejukomp
+{
+    public partial class Form1 : Form
+    {
+        double atlag;
+        int osszeg;
+        int min;
+        int max;
+        int db;
+        List<int> lista = new List<int>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            Random r = new Random();
+            int i = r.Next(0, 100);
+
+
+            Label lb = new Label();
+            lb.Location = new Point(e.X, e.Y);
+            lb.Text = i.ToString();
+            lb.AutoSize = true;
+            lb.Text = i.ToString();
+            Controls.Add(lb);
+            db++;
+            osszeg = osszeg+i;
+            atlag = osszeg / db;
+            lista.Add(i);
+            min = lista.Min();
+            max = lista.Max();
+
+            label1.Text = String.Format("db {0} , osszeg:{1},atlag:{2},min:{3},max{4}", db, osszeg, atlag, min, max);
+            
+
+            
+
+
+
+        }
+    }
+}
